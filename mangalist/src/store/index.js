96752593056c65bba2ff4c5d18d029d10cb5b9e9
sai.he1 <sai.he1@pactera.com>
2020-10-23 17:46:08 +0900

    debug

--- mangalist/src/store/index.js
+++ mangalist/src/store/index.js
@@ -2,7 +2,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-
 // Vuexの利用宣言
 Vue.use(Vuex)
 
@@ -13,5 +12,4 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     strict: true,
     modules: {}
-  }
-)
+})

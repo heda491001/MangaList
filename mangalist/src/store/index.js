f16111923b3cef3beed71847d56350d770dcf1e6
sai.he1 <sai.he1@pactera.com>
2020-10-23 17:22:06 +0900

    import vuex

---
+++ mangalist/src/store/index.js
@@ -0,0 +1,17 @@
+// Import Vue
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+// Vuexの利用宣言
+Vue.use(Vuex)
+
+/*
+  Store Regstration
+  ストアにモジュールを登録
+*/
+export default new Vuex.Store({
+    strict: true,
+    modules: {}
+  }
+)

a2a8e8b84fbee29300cb8df0c314f7183627de69
sai.he1 <sai.he1@pactera.com>
2020-10-27 18:37:26 +0900

    debug

--- mangalist/src/App.vue
+++ mangalist/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <p>update time 2020/10/27 17:35</p>  
+    <p>update time 2020/10/27 18:37</p>  
     <router-link to="/HelloWorld">HelloWorld</router-link>
     <router-link to="/add">add</router-link>
     <router-link to="/list">list</router-link>
